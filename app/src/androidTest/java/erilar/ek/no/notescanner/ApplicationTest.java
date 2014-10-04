d262f9706c9743b2ad2761e2d47be2c9e19f0cde
Simon Andresen <simon.andresen@gmail.com>
2014-10-04 22:16:23 +0200

    Added barebone skeleton

---
+++ app/src/androidTest/java/erilar/ek/no/notescanner/ApplicationTest.java
@@ -0,0 +1,10 @@
+package erilar.ek.no.notescanner;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
